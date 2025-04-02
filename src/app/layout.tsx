e439b564f2ed28f099644e42d359206159ebc082
alireza kargar <alirezakargarsuport@gmail.com>
2025-04-02 19:29:43 +0330

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata = {
     'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
   keywords: 'react,material,kit,application,dashboard,admin,template',
   themeColor: '#000000',
-  manifest: '/manifest.json',
+  // manifest: '/manifest.json',
   viewport: { width: 'device-width', initialScale: 1, maximumScale: 1 },
   icons: [
     { rel: 'icon', url: '/favicon/favicon.ico' },
